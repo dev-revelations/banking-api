25a0969e89362a20dcb11fdb4e29570262007ceb
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-30 04:16:34 +0100

    Fixing the 'misconfigured csrf' error

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.use(helmet());
+  app.use(session({
+    secret: 'temp secret for now',
+    saveUninitialized: true,
+    resave: true
+  }));
   app.use(csurf());
 
   await app.listen(3000);
