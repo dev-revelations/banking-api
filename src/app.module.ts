6583b208c4c17461c00ecf122038c9f839247d17
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-30 11:48:19 +0100

    Customer module generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { CustomerModule } from './modules/customer/customer.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
