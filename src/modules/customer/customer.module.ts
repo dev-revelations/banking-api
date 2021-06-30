aef1053dc24535f6a83fdc2042ce332845dcbca7
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-30 14:33:20 +0100

    Adding InMemoryDBModule to the customer module to avoid dependency resolve error

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import CustomerRepository from './repositories/customer.repository';
+import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 
 @Module({
+  imports: [
+    InMemoryDBModule.forFeature('customer')
+  ],
   controllers: [CustomerController],
-  providers: [CustomerService]
+  providers: [CustomerService, CustomerRepository]
 })
-export class CustomerModule {}
+export class CustomerModule { }
