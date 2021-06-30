a0e6ecdd8ca0293d2ceefa6135f1f4b87b4490ea
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-30 17:59:36 +0100

    Defining the Account creation DTO

--- src/modules/account/dto/create-account.dto.ts
+++ src/modules/account/dto/create-account.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAccountDto {}
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateAccountDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    customerId: string;
+}
